e661c3ce2d4d49e25e4cb837e8d3cf7880f0e7fe
Shubham paralikar <shubham.paralikar@gmail.com>
2019-05-03 10:40:03 +0530

    Add files via upload

---
+++ BLE_server_test/BLE_server_test.ino
@@ -0,0 +1,48 @@
+/*
+    Based on Neil Kolban example for IDF: https://github.com/nkolban/esp32-snippets/blob/master/cpp_utils/tests/BLE%20Tests/SampleServer.cpp
+    Ported to Arduino ESP32 by Evandro Copercini
+    updates by chegewara
+*/
+
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEServer.h>
+BLECharacteristic *pCharacteristic;
+// See the following for generating UUIDs:
+// https://www.uuidgenerator.net/
+
+#define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
+#define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Starting BLE work!");
+
+  BLEDevice::init("BLE device(ESP32)");
+  BLEServer *pServer = BLEDevice::createServer();
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+  pCharacteristic = pService->createCharacteristic(
+                                         CHARACTERISTIC_UUID,
+                                         BLECharacteristic::PROPERTY_READ |
+                                         BLECharacteristic::PROPERTY_WRITE
+                                       );
+
+  pCharacteristic->setValue("Hello From ESP");
+  // pCharacteristic->setValue("Bluetooth");
+  pService->start();
+  // BLEAdvertising *pAdvertising = pServer->getAdvertising(); 
+  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+  pAdvertising->addServiceUUID(SERVICE_UUID);
+  pAdvertising->setScanResponse(true);
+  pAdvertising->setMinPreferred(0x06); 
+  pAdvertising->setMinPreferred(0x12);
+  BLEDevice::startAdvertising();
+  Serial.println("Characteristic defined! Now you can read it in your phone!");
+}
+
+void loop() {
+pCharacteristic->setValue("chargingState: true ; Latitude: \"15.7383647\" ; Longitude: \"17.463647\" ; Time: \"tue, March 26 1:02:08 GMT+05:30 2019\";");
+pCharacteristic->notify();
+BLEDevice::startAdvertising();
+  delay(2000);
+}
